b23a71a70e81fa6acd9db268cafb6a2dd5455c65
Ishan Goel <igoel.mail@gmail.com>
2022-01-25 11:56:24 -0800

    dracula

--- renderer.go
+++ renderer.go
@@ -484,7 +484,7 @@ func (r *renderer) renderCodeBlock(w io.Writer, node *ast.CodeBlock) {
 
 	buf := &bytes.Buffer{}
 
-	err = formatter.Format(buf, styles.Pygments, iterator)
+	err = formatter.Format(buf, styles.Dracula, iterator)
 	if err != nil {
 		// Something failed, falling back to no highlight render
 		r.renderFormattedCodeBlock(w, code)
